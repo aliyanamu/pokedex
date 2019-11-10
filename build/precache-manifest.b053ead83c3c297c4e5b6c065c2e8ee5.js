ba0b5427dc360c9e2840cb2c056a6b84ca2a27ad
hana <aliyanamu@gmail.com>
2019-11-10 13:44:31 +0700

    release build

---
+++ build/precache-manifest.b053ead83c3c297c4e5b6c065c2e8ee5.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685b7636f1d5b5b2d5e4c740c234f7ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2df7e95c08aa4784f968",
+    "url": "/static/css/2.d5d05702.chunk.css"
+  },
+  {
+    "revision": "7e0cec8eb6a0b752777d",
+    "url": "/static/css/main.544750ef.chunk.css"
+  },
+  {
+    "revision": "2df7e95c08aa4784f968",
+    "url": "/static/js/2.2cd1914c.chunk.js"
+  },
+  {
+    "revision": "7e0cec8eb6a0b752777d",
+    "url": "/static/js/main.91679697.chunk.js"
+  },
+  {
+    "revision": "5ef2c9768c4093f8fa6a",
+    "url": "/static/js/runtime-main.8e55a701.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "407b37120c31b8480c4565c5090090b3",
+    "url": "/static/media/cave.407b3712.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "4c199b5ae48a4a8b8f1ae957e374d279",
+    "url": "/static/media/forest.4c199b5a.jpg"
+  },
+  {
+    "revision": "bf8946953646db6ec7238f54d2b0e9de",
+    "url": "/static/media/grassland.bf894695.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "4eed3c2157afe50968b025a543bcc7b5",
+    "url": "/static/media/mountain.4eed3c21.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd4563672099d43763327042139b4a4b",
+    "url": "/static/media/pokeball.cd456367.png"
+  },
+  {
+    "revision": "abe8039a4b32bc557ed1f012a61e6fd2",
+    "url": "/static/media/rare.abe8039a.jpg"
+  },
+  {
+    "revision": "a4b82b5fa1d1126887ef8cb8046687cf",
+    "url": "/static/media/rough-terrain.a4b82b5f.jpg"
+  },
+  {
+    "revision": "6a996d22a220b1e33fd6ff66509201ff",
+    "url": "/static/media/sea.6a996d22.jpg"
+  },
+  {
+    "revision": "073eeab64d753652b5d16b7e1cb3a6de",
+    "url": "/static/media/urban.073eeab6.jpg"
+  },
+  {
+    "revision": "324cdfd28abc60abe1498b0db8bdf592",
+    "url": "/static/media/waters-edge.324cdfd2.jpg"
+  }
+]);
